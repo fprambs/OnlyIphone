fd39db7c69b117c02d10729c3c6ef54be24a847c
Felipe Prambs <felipe._16live.com>
2017-11-06 15:36:17 -0300

    Coneccion a base de datos

---
+++ OnlyIphone/Connection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace OnlyIphone
+{
+    class Connection
+    {
+        public static MySqlConnection getConnection()
+        {
+            MySqlConnection connect = new MySqlConnection("server=127.0.0.1; database=onlyiphone; Uid=root; pwd=;");
+
+            connect.Open();
+            return connect;
+        }
+    }
+}
---
+++ OnlyIphone/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnlyIphone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text.Trim();
+            string password = textBox2.Text.Trim();
+
+            User user = new User();
+
+            user = Controller.getUser(username);
+
+            if (user.Password == password)
+            {
+                MessageBox.Show("Ingreso Exitoso", "Información!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("username y/o contraseña incorrecta", "Información", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+    }
+}
