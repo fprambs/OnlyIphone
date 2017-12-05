f17c91fbffb34c62ee9bfa525cdb9edcbd1cc9ff
fprambs <felipe._16@live.com>
2017-12-05 13:39:01 -0300

    Add files via upload

---
+++ onlyiphone(1).sql
@@ -0,0 +1,369 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-12-2017 a las 20:38:31
+-- Versión del servidor: 10.1.26-MariaDB
+-- Versión de PHP: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `onlyiphone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cashbox`
+--
+
+CREATE TABLE `cashbox` (
+  `date` date NOT NULL,
+  `income` int(11) NOT NULL,
+  `outcome` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `client`
+--
+
+CREATE TABLE `client` (
+  `rut` varchar(9) NOT NULL,
+  `names` tinytext,
+  `last_names` tinytext,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `address` tinytext
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detail`
+--
+
+CREATE TABLE `detail` (
+  `id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `total_price` int(11) NOT NULL,
+  `sale` int(11) NOT NULL,
+  `product` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `outcome`
+--
+
+CREATE TABLE `outcome` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `value` int(11) NOT NULL,
+  `description` tinytext NOT NULL,
+  `username` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `product`
+--
+
+CREATE TABLE `product` (
+  `id` varchar(6) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `provider` int(11) NOT NULL,
+  `category` int(11) NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provider`
+--
+
+CREATE TABLE `provider` (
+  `id` int(11) NOT NULL,
+  `name` tinytext NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` tinytext NOT NULL,
+  `city` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `provider`
+--
+
+INSERT INTO `provider` (`id`, `name`, `phone`, `email`, `address`, `city`) VALUES
+(1, 'Proveedor 1', 956473856, 'proveedor1@gmail.com', 'Carlos Krahmer 2277', 'Valdivia'),
+(2, 'Proveedor 2', 946374655, 'proveedor2@gmail.com', 'Pedro Montt 4328', 'Temuco'),
+(3, 'Proveedor 3', 964537284, 'proveedor3@gmail.com', 'Anibal Pinto 3423', 'Valdivia'),
+(4, 'Proveedor 4', 956473645, 'proveedor4@gmail.com', 'Los Alercer 3645', 'Puerto Montt'),
+(5, 'Proveedor 5', 956473846, 'proveedor5@gmail.com', 'Muñoz Hermosilla 5647', 'La Serena');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sale`
+--
+
+CREATE TABLE `sale` (
+  `id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `cashbox_date` date NOT NULL,
+  `client` varchar(9) NOT NULL,
+  `username` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `type`
+--
+
+CREATE TABLE `type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `type`
+--
+
+INSERT INTO `type` (`id`, `name`) VALUES
+(1, 'Carcasa');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(15) NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`username`, `password`) VALUES
+('admin', 'MQAyADMANAA=');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `work_order`
+--
+
+CREATE TABLE `work_order` (
+  `id` int(11) NOT NULL,
+  `item` varchar(100) NOT NULL,
+  `date` date NOT NULL,
+  `description` text NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `client` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cashbox`
+--
+ALTER TABLE `cashbox`
+  ADD PRIMARY KEY (`date`);
+
+--
+-- Indices de la tabla `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`rut`);
+
+--
+-- Indices de la tabla `detail`
+--
+ALTER TABLE `detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sale` (`sale`),
+  ADD KEY `product` (`product`);
+
+--
+-- Indices de la tabla `outcome`
+--
+ALTER TABLE `outcome`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indices de la tabla `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `provider` (`provider`),
+  ADD KEY `categorye` (`category`),
+  ADD KEY `type` (`type`);
+
+--
+-- Indices de la tabla `provider`
+--
+ALTER TABLE `provider`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sale`
+--
+ALTER TABLE `sale`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client` (`client`),
+  ADD KEY `client_2` (`client`),
+  ADD KEY `username` (`username`),
+  ADD KEY `cashbox_date` (`cashbox_date`);
+
+--
+-- Indices de la tabla `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indices de la tabla `work_order`
+--
+ALTER TABLE `work_order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`),
+  ADD KEY `client` (`client`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detail`
+--
+ALTER TABLE `detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `outcome`
+--
+ALTER TABLE `outcome`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `provider`
+--
+ALTER TABLE `provider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `sale`
+--
+ALTER TABLE `sale`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `work_order`
+--
+ALTER TABLE `work_order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detail`
+--
+ALTER TABLE `detail`
+  ADD CONSTRAINT `detail_ibfk_1` FOREIGN KEY (`product`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `detail_ibfk_2` FOREIGN KEY (`sale`) REFERENCES `sale` (`id`);
+
+--
+-- Filtros para la tabla `outcome`
+--
+ALTER TABLE `outcome`
+  ADD CONSTRAINT `outcome_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+
+--
+-- Filtros para la tabla `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category`) REFERENCES `category` (`id`),
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`type`) REFERENCES `type` (`id`),
+  ADD CONSTRAINT `product_ibfk_3` FOREIGN KEY (`provider`) REFERENCES `provider` (`id`);
+
+--
+-- Filtros para la tabla `sale`
+--
+ALTER TABLE `sale`
+  ADD CONSTRAINT `sale_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  ADD CONSTRAINT `sale_ibfk_2` FOREIGN KEY (`client`) REFERENCES `client` (`rut`),
+  ADD CONSTRAINT `sale_ibfk_3` FOREIGN KEY (`cashbox_date`) REFERENCES `cashbox` (`date`);
+
+--
+-- Filtros para la tabla `work_order`
+--
+ALTER TABLE `work_order`
+  ADD CONSTRAINT `work_order_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  ADD CONSTRAINT `work_order_ibfk_2` FOREIGN KEY (`client`) REFERENCES `client` (`rut`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
